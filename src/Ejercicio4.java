21e9b2066c575369b90172a56f1e9444a47045d0
Miguel A. Olmedo <miguelangelolmedo24@outlook.es>
2025-08-21 19:57:15 -0600

    Ejercicio4.java

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -1,5 +1,6 @@
 public class Ejercicio4 {
 	public static void main(String[] args) {
-		System.out.println("---\n\nP: Programacion\n0: Orientada a\nO: Objetos\n\n---\n\nC:\\Users\\PC\\Desktop\\POO-EJERCICIOS>");
+		System.out.println("---\n\nP: Programacion\nO: Orientada a\nO: Objetos\n\n---\n\n");
 	}
+
 } 
