dcfba38a01a51e337e7031b69386c3bf60cf9c6b
Miguel A. Olmedo <miguelangelolmedo24@outlook.es>
2025-08-21 19:37:13 -0600

    Ejecutable.bat

---
+++ Ejecutable.bat
@@ -0,0 +1,26 @@
+@echo off
+echo ==== EJECUCION DE TODOS LOS PROGRAMAS ====
+echo.
+
+echo 1. MACO - Iniciales:
+java -cp bin MACO
+echo.
+
+echo 2. Datos Primitivos:
+java -cp bin DatosPrimitivos
+echo.
+
+echo 3. Computadora:
+java -cp bin Computadora
+echo.
+
+echo 4. Ejercicio POO - Texto:
+java -cp bin Ejercicio4
+echo.
+
+echo 5. Ejercicio5 - Constanstes y Variables:
+java -cp bin Ejercicio5
+echo.
+
+echo ==== EJECUCION COMPLETADA ====
+pause
